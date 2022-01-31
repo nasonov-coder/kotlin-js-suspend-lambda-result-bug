bd22e81bcbe368ef67d1638934727d7f7d0731ca
dmitry <nasonov.coder@gmail.com>
2022-01-31 22:16:21 +0300

    result to value class

--- src/main/kotlin/Simple.kt
+++ src/main/kotlin/Simple.kt
@@ -1,4 +1,6 @@
+
 suspend fun main() {
-    val lambda: suspend () -> Result<String> = { Result.success("suc") }
-    println(lambda.invoke().getOrThrow())
+    val lambda: suspend () -> Res = { Res("asd") }
+    println(lambda.invoke())
 }
+value class Res(val value: String)
